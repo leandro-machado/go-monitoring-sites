5a2c59983bb79cbb729bba3026febdc3666d6778
Leandro Machado <lemachadosantos18@gmail.com>
2020-12-10 18:35:58 -0300

    feat: added log (files)

--- hello.go
+++ hello.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 const monitorings = 2
-const delay = 3
+const delay = 1
 
 func main() {
 	showIntro()
@@ -23,6 +25,7 @@ func main() {
 			initMonitoring()
 		case 2:
 			fmt.Println("Show logs ...")
+			showLog()
 		case 0:
 			os.Exit(0)
 		default:
@@ -37,7 +40,8 @@ func showIntro() {
 	fmt.Println("Hi, what's your name?")
 	fmt.Scan(&name)
 	fmt.Println("---------------------")
-	fmt.Println("Hi,", name, "Select one action:")
+	fmt.Println("Hi,", name)
+	fmt.Println("Select one action:")
 }
 
 func showMenu() {
@@ -70,10 +74,38 @@ func initMonitoring() {
 }
 
 func testSite(site string) {
-	res, _ := http.Get(site)
+	res, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
 	if res.StatusCode == 200 {
 		fmt.Println(site, "it's work!")
+		createLog(site, true)
 	} else {
 		fmt.Println(site, "not work :(")
 	}
+}
+
+func createLog(site string, status bool) {
+	file, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Error:", file)
+	}
+
+	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	file.Close()
+}
+
+func showLog() {
+	file, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(string(file))
 }
