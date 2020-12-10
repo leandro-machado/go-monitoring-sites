b8c292742f76980ac99da79a6d579ec68e4bb490
Leandro Machado <lemachadosantos18@gmail.com>
2020-12-10 16:40:22 -0300

    feat: imported os package and intro on funcs

--- hello.go
+++ hello.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	name := "Leandro Machado"
-	fmt.Println("Hi,", name)
+	showIntro()
+	showMenu()
 
-	fmt.Println("1 - Init monitoring")
-	fmt.Println("2 - Show logs")
-	fmt.Println("0 - Exit")
-
-	var command int
-
-	fmt.Scan(&command)
+	command := readCommand()
 
 	switch command {
 	case 1:
@@ -22,8 +17,30 @@ func main() {
 	case 2:
 		fmt.Println("Show logs ...")
 	case 0:
-		fmt.Println("Exiting")
+		os.Exit(0)
 	default:
 		fmt.Println("Command not found")
+		os.Exit(-1)
 	}
+}
+
+func showIntro() {
+	var name string
+	fmt.Println("Hi, what's your name?")
+	fmt.Scan(&name)
+	fmt.Println("---------------------")
+	fmt.Println("Hi,", name)
+}
+
+func showMenu() {
+	fmt.Println("1 - Init monitoring")
+	fmt.Println("2 - Show logs")
+	fmt.Println("0 - Exit")
+}
+
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
+
+	return command
 }
